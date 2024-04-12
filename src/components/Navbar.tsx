2c577caeac657c7da04ce64dbf64b60461075160
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-12 04:54:29 +0100

    Adde icons to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faInfoCircle, faSearch, faShare } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="navbar">
       <FontAwesomeIcon icon={faBars} />
+      <p>Dashboard</p>
+      <div>
+        <FontAwesomeIcon icon={faShare} />
+        <FontAwesomeIcon icon={faSearch} />
+        <FontAwesomeIcon icon={faInfoCircle} />
+      </div>
     </div>
   )
 }
