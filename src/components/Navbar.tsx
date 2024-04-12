4cb6c78d2b2e1621a14c231dbbdd716e8a560d8b
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-12 04:42:46 +0100

    Added Hamburger menu to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <FontAwesomeIcon icon={faBars} />
+    </div>
   )
 }
 
