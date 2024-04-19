2d4b9c150b3a71e2c4f80de687420676c534b4c6
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-19 12:43:26 +0100

    Added socila media icons to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <div className='footer'>
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faInstagram} />
+        </div>
+    )
 }
 
 export default Footer
