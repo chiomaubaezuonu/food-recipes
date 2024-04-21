fa6eb9a059ced4984e704f37042c6897f6e780b8
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-22 00:11:58 +0100

    Added and styled the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,8 @@
-import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
     return (
         <div className='footer'>
-            <a href="https://web.facebook.com/reel/624830593097899"><FontAwesomeIcon className='fb' icon={faFacebook} /></a>
-            <a href="https://twitter.com/ChiomaPresh1029"><FontAwesomeIcon icon={faTwitter} /></a>
-            <a href="https://www.linkedin.com/in/chioma-ubaezuonu-924b1214b/"> <FontAwesomeIcon icon={faLinkedin} /></a>
-            <FontAwesomeIcon icon={faInstagram} />
+            <p>&copy;  2024 <span style={{color:'#645cff'}}>SimplyRecipes</span>  Built by <a href="https://chomydev-portfolio.netlify.app/"><span style={{color:'#645cff'}}>Omacodes</span></a> </p>
         </div>
     )
 }
