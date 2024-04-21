ae1ff2733480c71fd9ebdd2ca2634f5806d99a83
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-21 19:45:17 +0100

    Deleted the whole section aside the Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import Navbar from "./components/Navbar.jsx"
-import Hero from './Hero.jsx'
-import Menu from './Menu.jsx'
+
 import Footer from './components/Footer.jsx'
+import Main from './components/Main.jsx'
 
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Hero />
-      <Menu />
+      <Main />
       <Footer />
     </div>
   )
