762f5e9330cc74f9699fd1efa42873e4ce80bc77
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-23 11:42:08 +0100

    Styled the Tags page

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -21,15 +21,17 @@ const tags = [
 const Tags = () => {
     return (
         <div className='tags-page'>
-            {
+           <div className="tags-container">
+           {
                 tags.map((tag) => (
-                    <div>
-                        <h2>{tag.tagName}</h2>
-                        <p>{tag.recipeCount}</p>
+                    <div className='tags-card'>
+                        <h5 className='tagName'>{tag.tagName}</h5>
+                        <p className='recipe-count'>{tag.recipeCount}</p>
                     </div>
 
                 ))
             }
+           </div>
         </div>
     )
 }
