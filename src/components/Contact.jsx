cb76a13bf1af3e7095146d3322bae4e5779d99d8
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-23 14:06:19 +0100

    Styled the contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,13 +4,35 @@ import {
     Input,
 } from 'antd';
 
+
+const recipeCards = [
+    {
+        img: "images/recipe-1.jpeg",
+        name: "Carne Asada",
+        prep: "15min | Cook : 5min"
+    },
+    {
+        img: "images/recipe-2.jpeg",
+        name: "Greek Ribs",
+        prep: "15min | Cook : 5min"
+    },
+    {
+        img: "images/recipe-3.jpeg",
+        name: "Vegetable Soup",
+        prep: "15min | Cook : 5min"
+    },
+]
 const Contact = () => {
     return (
         <div className='contact-page'>
             <div className='contact-page-contents'>
                 <div className="contact-writeup">
-                    <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, assumenda.</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nulla modi dolorem tenetur qui aliquid vero recusandae temporibus animi eaque quam in id, ipsa odio sit beatae dolores! Sed provident vitae fugiat eos ex delectus culpa, voluptatum, vel iste expedita libero, neque possimus ipsum enim qui. Corporis eveniet nihil omnis quam assumenda nulla blanditiis provident ullam exercitationem. Ipsa, ut porro.</p>
+                    <h3 className='contact-title'>Want To Get In Touch?</h3>
+                    <p>Four dollar toast biodiesel plaid salvia actually pickled banjo bespoke mlkshk intelligentsia edison bulb synth.</p>
+
+                    <p>Cardigan prism bicycle rights put a bird on it deep v.</p>
+
+                    <p>Hashtag swag health goth air plant, raclette listicle fingerstache cold-pressed fanny pack bicycle rights cardigan poke.</p>
 
 
                 </div>
@@ -68,6 +90,20 @@ const Contact = () => {
                     </Form>
                 </div>
 
+            </div>
+            <div className='card-content-div'>
+                <h4 className='about-subtitle'>Look At This Awesomesouce!</h4>
+                <div className="card-container">
+                    {
+                        recipeCards.map((card) => (
+                            <div className='card'>
+                                <img className='card-img' src={card.img} alt='food' />
+                                <h4 className='card-name'>{card.name}</h4>
+                                <p className='card-text'>{card.prep}</p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
