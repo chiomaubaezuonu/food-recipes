a25598b6a4e0a8b96dfdd2e8267faebaba216c51
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-23 23:36:13 +0100

    Added the instruction section of the pancake page

--- src/components/Pancake.jsx
+++ src/components/Pancake.jsx
@@ -1,4 +1,6 @@
 import { faClock } from "@fortawesome/free-regular-svg-icons"
+import { faUserFriends } from "@fortawesome/free-solid-svg-icons"
+import { faUsers } from "@fortawesome/free-solid-svg-icons/faUsers"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { NavLink } from "react-router-dom"
 
@@ -17,12 +19,12 @@ const Pancake = () => {
                             <p>30 min.</p>
                         </div>
                         <div>
-                        <FontAwesomeIcon icon={faClock} />
+                            <FontAwesomeIcon icon={faClock} />
                             <p>Prep Time</p>
                             <p>15 min.</p>
                         </div>
                         <div>
-                        <FontAwesomeIcon icon={faClock} />
+                            <FontAwesomeIcon icon={faUserFriends} />
                             <p>Serving</p>
                             <p>6 Servings.</p>
                         </div>
@@ -34,6 +36,34 @@ const Pancake = () => {
                         <NavLink to="/"><button>Pancakes</button></NavLink>
                         <NavLink to="/"><button>Food</button></NavLink>
                     </div>
+                </div>
+            </div>
+            <div className="guideline-section">
+                <div className="instructions-section">
+                    <div className="step-one">
+                        <p>Step 1</p>
+                        <div></div>
+                        <p>I'm baby mustache man braid fingerstache small batch venmo succulents shoreditch.</p>
+                    </div>
+                    <div className="step-two">
+                        <p>Step 2</p>
+                        <div style={{
+                            height: '2px',
+                            background: "#cbd5e1"
+                        }}></div>
+                        <p>
+                            Pabst pitchfork you probably haven't heard of them, asymmetrical seitan tousled succulents wolf banh mi man bun bespoke selfies freegan ethical hexagon.
+
+                        </p>
+                    </div>
+                    <div className="step-three">
+                        <p>Step 3</p>
+                        <div></div>
+                        <p>Polaroid iPhone bitters chambray. Cornhole swag kombucha live-edge.</p>
+                    </div>
+                </div>
+                <div className="ingredients-section">
+
                 </div>
             </div>
         </div>
