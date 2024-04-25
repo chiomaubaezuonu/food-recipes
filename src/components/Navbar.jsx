7c8b955ab0e2d6d785acc4bbd52379d265287afd
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-04-25 12:03:28 +0100

    Dade about page responsive on mobile device

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,35 +8,35 @@ const Navbar = () => {
   return (
     <div>
       <div className='nav-container'>
-      <div className='logo.container'>
-        <NavLink to='/'> <img className='logo' src='./images/logo.svg' alt='logo' /></NavLink>
+        <div className='logo.container'>
+          <NavLink to='/'> <img className='logo' src='./images/logo.svg' alt='logo' /></NavLink>
+        </div>
+        <div className='navbar-links'>
+          <div className='a-tags'>
+            <NavLink to="/" className='nav-link'>Home</NavLink>
+            <NavLink to="/about" className='nav-link'>About</NavLink>
+            <NavLink to="tags" className='nav-link'>Tags</NavLink>
+            <NavLink to="recipes" className='nav-link'>Recipes</NavLink>
+          </div>
+          <div className='btn-div'>
+            <NavLink to="/contact"> <button className='nav-btn'>Contact</button></NavLink>
+          </div>
+        </div>
+        <img onClick={() => setOpenMenuList(!openMenuList)} className='menu' src='./images/hamburger.png' alt='menu icon' />
       </div>
-      <div className='navbar-links'>
-        <div className='a-tags'>
-          <NavLink to="/" className='nav-link'>Home</NavLink>
-          <NavLink to="/about" className='nav-link'>About</NavLink>
-          <NavLink to="tags" className='nav-link'>Tags</NavLink>
-          <NavLink to="recipes" className='nav-link'>Recipes</NavLink>
+      {openMenuList &&
+        <div className='navbar-links small'>
+          <div className='a-tags'>
+            <NavLink to="/" className='nav-link' onClick={() => setOpenMenuList(false)}>Home</NavLink>
+            <NavLink to="/about" className='nav-link' onClick={() => setOpenMenuList(false)}>About</NavLink>
+            <NavLink to="tags" className='nav-link' onClick={() => setOpenMenuList(false)}>Tags</NavLink>
+            <NavLink to="recipes" className='nav-link' onClick={() => setOpenMenuList(false)}>Recipes</NavLink>
+          </div>
+          <div className='btn-div'>
+            <NavLink to="/contact"> <button className='nav-btn' onClick={() => setOpenMenuList(false)}>Contact</button></NavLink>
+          </div>
         </div>
-        <div className='btn-div'>
-          <NavLink to="/contact"> <button className='nav-btn'>Contact</button></NavLink>
-        </div>
-      </div>
-      <img onClick={() => setOpenMenuList(!openMenuList)} className='menu' src='./images/hamburger.png' alt='menu icon' />
-    </div>
-     {openMenuList &&
-      <div className='navbar-links small'>
-        <div className='a-tags'>
-          <NavLink to="/" className='nav-link'>Home</NavLink>
-          <NavLink to="/about" className='nav-link'>About</NavLink>
-          <NavLink to="tags" className='nav-link'>Tags</NavLink>
-          <NavLink to="recipes" className='nav-link'>Recipes</NavLink>
-        </div>
-        <div className='btn-div'>
-          <NavLink to="/contact"> <button className='nav-btn'>Contact</button></NavLink>
-        </div>
-      </div>
-    }
+      }
     </div>
   )
 }
